33ae7fdfa45f4787b6e404b01ba71db839a4b86c
Isuru619 <icitu619@gmail.com>
2015-04-09 16:57:28 +0530

    Draw Table

--- Table.pl
+++ Table.pl
@@ -1,12 +1,12 @@
-/*operators*/
+
 :- op(1000,xfy,'and').
 :- op(1000,xfy,'or').
 :- op(900,fy,'not').
 
-discover _vars(N,V,V) :- member(N,[0,1]),!.    /* Boolean constants in expression */
+discover _vars(N,V,V) :- member(N,[0,1]),!.
 discover_vars(X,Vin,Vout) :- atom(X), 
-                         (member(X,Vin) -> Vout = Vin ;   /* already have  */
-                            Vout = [X|Vin]).                 /* include           */
+                         (member(X,Vin) -> Vout = Vin ;
+                            Vout = [X|Vin]).
 discover_vars(X and Y,Vin,Vout) :- discover_vars(X,Vin,Vtemp),
                                discover_vars(Y,Vtemp,Vout).
 discover_vars(X or Y,Vin,Vout) :-  discover_vars(X,Vin,Vtemp),
@@ -38,4 +38,28 @@ true_value(not X,Vars,A,Val) :-   true_value(X,Vars,A,VX),
 
 lookup(X,[X|_],[V|_],V).
 lookup(X,[_|Vars],[_|A],V) :- lookup(X,Vars,A,V).
+tt(E) :- find_vars(E,[],V),
+         reverse(V,Vars),
+         initial_assign(Vars,A),
+         write('  '), write(Vars), write('    '), write(E), nl,
+         write('-----------------------------------------'), nl,
+         write_row(E,Vars,A),
+         write('-----------------------------------------'), nl.
+
+write_row(E,Vars,A) :- write('  '), write(A), write('        '), 
+                       truth_value(E,Vars,A,V), write(V), nl,
+                       (successor(A,N) -> write_row(E,Vars,N) ; true).
+
+
+boole_and(0,0,0).      
+boole_or(0,0,0).      
+boole_not(0,1).
+boole_and(0,1,0).      
+boole_or(0,1,1).      
+boole_not(1,0).
+boole_and(1,0,0).      
+boole_or(1,0,1).
+boole_and(1,1,1).      
+boole_or(1,1,1).
+
 
