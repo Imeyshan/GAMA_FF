09a38974d6eb1f9156650cdca31756d252c9ea7f
Imeyshan <imeyshan@live.com>
2015-04-10 13:53:48 +0530

    first_assign

--- Table.pl
+++ Table.pl
@@ -12,3 +12,14 @@ discover_vars(X and Y,Vin,Vout) :- discover_vars(X,Vin,Vtemp),
 discover_vars(X or Y,Vin,Vout) :-  discover_vars(X,Vin,Vtemp),
                                discover_vars(Y,Vtemp,Vout).
 discover_vars(not X,Vin,Vout) :-   discover_vars(X,Vin,Vout).
+
+first_assign([],[]).
+first_assign([X|R],[0|S]) :- first_assign(R,S).
+
+successor(A,S) :- reverse(A,R),
+                  next(R,N),
+                  reverse(N,S).
+
+
+next([0|R],[1|R]).
+next([1|R],[0|S]) :- next(R,S).
