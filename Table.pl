f7625d8ada0e4025919549892ced6fa8631f897f
Imeyshan <imeyshan@live.com>
2015-04-10 13:53:17 +0530

    discover _vars

--- Table.pl
+++ Table.pl
@@ -2,3 +2,13 @@
 :- op(1000,xfy,'and').
 :- op(1000,xfy,'or').
 :- op(900,fy,'not').
+
+discover _vars(N,V,V) :- member(N,[0,1]),!.    /* Boolean constants in expression */
+discover_vars(X,Vin,Vout) :- atom(X), 
+                         (member(X,Vin) -> Vout = Vin ;   /* already have  */
+                            Vout = [X|Vin]).                 /* include           */
+discover_vars(X and Y,Vin,Vout) :- discover_vars(X,Vin,Vtemp),
+                               discover_vars(Y,Vtemp,Vout).
+discover_vars(X or Y,Vin,Vout) :-  discover_vars(X,Vin,Vtemp),
+                               discover_vars(Y,Vtemp,Vout).
+discover_vars(not X,Vin,Vout) :-   discover_vars(X,Vin,Vout).
